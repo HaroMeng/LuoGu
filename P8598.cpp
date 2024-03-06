fb0a845c8ef899d8bd110e40349e78dd8cef2d84
yulinguogu <3400484626@qq.com>
2024-03-06 21:25:23 +0800

    yes

---
+++ P8598.cpp
@@ -0,0 +1,18 @@
+// 问题连接:https://www.luogu.com.cn/problem/P8598
+#include <iostream>
+using namespace std;
+#define Max 100 //定义最多有100行100列
+
+int main()
+{
+    int x,y;
+    cin>>x;
+    // 构造一个循环用于输入数据
+    for(int i = 0;i < x; i++) {
+        for(int j = 0;j < Max; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    return 0;
+}
