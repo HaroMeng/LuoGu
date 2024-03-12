bb7669e2563d25b595e472abe825aeb184f10167
yulinguogu <3400484626@qq.com>
2024-03-12 19:27:29 +0800

    deleted:    ../.vscode/setting.json 	modified:   ../P8598.cpp

--- P8598.cpp
+++ P8598.cpp
@@ -1,18 +1,31 @@
 // 问题连接:https://www.luogu.com.cn/problem/P8598
 #include <iostream>
+#include <algorithm>
 using namespace std;
-#define Max 100 //定义最多有100行100列
+
+const int N = 10005;
+int n;
+int a[N];
+int cnt;
+int ans1, ans2;
 
 int main()
 {
-    int x,y;
-    cin>>x;
-    // 构造一个循环用于输入数据
-    for(int i = 0;i < x; i++) {
-        for(int j = 0;j < Max; j++) {
-            cin >> a[i][j];
-        }
+    cin >> n;
+    while (cin >> a[cnt])
+        cnt++;
+
+    sort(a, a + cnt);
+
+    for (int i = 0; i < cnt; i++)
+    {
+        if (a[i] == a[i + 1])
+            ans2 = a[i];
+        if (a[i + 1] - a[i] > 1)
+            ans1 = a[i] + 1;
     }
+
+    cout << ans1 << " " << ans2 << endl;
 
     return 0;
 }
