05e3b753274fcd92e437cd60f9ed2de5b58bf89f
HaroMeng <yulinguogu@qq.com>
2024-07-30 15:48:32 +0800

    0730finish

---
+++ demo/P1089.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int mon[12];
+    for (int i = 0; i < 12; i++)
+        cin >> mon[i];
+    int free, sum = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        int money = 300;
+        free = money - mon[i];
+    }
+    
+    return 0;
+}
---
+++ demo/P1421.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int a, b, x = 0;
+    cin >> a >> b;
+    x = (a * 10 + b) / 19;
+    cout << x;
+}
